19aa5d395faca19be1156650d2cdbb4a895d0a02
santixo16 <santixo2022@gmail.com>
2022-07-19 00:38:43 -0500

    added multer

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('Hola mundooo')
+})
+
+app.post('/imagen', upload.single('imagen'),(req, res) => {
+    const body = req.body
+    const imagen = req.file
+    console.log(imagen)
+    res.send('Hello from POST')  
 })
 
 app.listen(port, () => {
